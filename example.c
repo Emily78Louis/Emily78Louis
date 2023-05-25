d1f7116277ea1eaf114e9c7e6f889e5df75bffe8
Emily78Louis <134580959+Emily78Louis@users.noreply.github.com>
2023-05-25 11:44:30 +0530

    Update example.c

--- example.c
+++ example.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Yes this is the best piece of C code,
+I have ever written in my life,
+You can probably understand what it does,
+by reading it!
+*/
+
 long int pow(long int a, long int b) {
     if (b == 0) {
         return 1;
@@ -12,7 +19,7 @@ long int pow(long int a, long int b) {
 char s[8];
 char a[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
-void Tiny_Overhead_write(long int* arg1) {
+void Write(long int* arg1) {
     if (*arg1 >= 4294967296) {
         printf("Error! %ld %s", *arg1, "is larger than 4294967295.");
         exit(1);
@@ -25,7 +32,7 @@ void Tiny_Overhead_write(long int* arg1) {
 }
 
 // Optimization Required here !
-long int Tiny_Overhead_read() {
+long int Read() {
     long int y = 0;
     for (int i = 8; i > 0; i--) {
         for (int j = 0; j < 0x10; j++) {
@@ -37,14 +44,11 @@ long int Tiny_Overhead_read() {
     return y;
 }
 
-int main() {
-    // printf("Pass!")
-    // Works like a charm
-    
+int main() {  
     long int x = 0;
     do {
-        Tiny_Overhead_write(&x);
-        if (x == Tiny_Overhead_read()) {
+        Write(&x);
+        if (x == Read()) {
             printf("Yes! at x: %ld\n", x);
         } else {
             printf("No! at x: %ld\n", x);
@@ -53,4 +57,5 @@ int main() {
         x++;
     } while (x < 4294967295);
     printf("Finished!\n");
+    return 0;
 }
